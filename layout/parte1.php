43de2dc38c388eb9f3837517fcba04f67dba17e9
Alberto <albertoluischevarria@gmail.com>
2024-11-01 08:35:06 -0500

    PLANTILLA OK

--- layout/parte1.php
+++ layout/parte1.php
@@ -29,7 +29,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index.php" class="nav-link">Sistema de Ventas</a>
+        <a href="<?php echo $URL; ?>/index.php" class="nav-link">Sistema de Ventas</a>
       </li>
       
     </ul>
@@ -51,8 +51,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index.php" class="brand-link">
-      <img src="public/images/ventas.jpg" alt="Ventas Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="<?php echo $URL; ?>/index.php" class="brand-link">
+      <img src="<?php echo $URL; ?>/public/images/ventas.jpg" alt="Ventas Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Sistema de Ventas</span>
     </a>
 
@@ -61,7 +61,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="public/images/fotoachk.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?php echo $URL; ?>/public/images/fotoachk.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block"><?php echo $nombres_sesion?></a>
